1187110e0e7556b9dd712af0b9dde13ca4fc1f3f
zeinmubaraq <zainmubaraq@gmail.com>
2022-04-16 07:47:33 +0700

    add blog feature using getServerSideProps

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,16 +1,44 @@
-import { NextPage } from 'next';
 import Layout from '../components/Layout';
 import Head from 'next/head';
 
-const BlogPage: NextPage = () => {
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
+
+interface BlogProps {
+  dataBlog: Post[];
+}
+
+export default function BlogPage(props: BlogProps) {
+  const { dataBlog } = props;
+
   return (
     <Layout>
       <Head>
         <title>Blog - NextJS Basic Typescript</title>
       </Head>
-      <h1 className="inline-block text-lg">Blogs Page</h1>
+      <h1 className="inline-block text-xl">Blogs Page</h1>
+      {dataBlog.map(blog => {
+        return (
+          <article key={blog.id} className="mt-5">
+            <h3 className="text-lg font-semibold capitalize">{blog.title}</h3>
+            <p className="text-sm">{blog.body}</p>
+          </article>
+        );
+      })}
     </Layout>
   );
-};
+}
 
-export default BlogPage;
+export async function getServerSideProps() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const dataBlog = await res.json();
+
+  return {
+    props: {
+      dataBlog,
+    },
+  };
+}
